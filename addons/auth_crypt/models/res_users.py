68982b6c4ab2499b0a61f4f6f5e5c55879b4403d
Anwari Rahman <anwari96@gmail.com>
2023-07-06 17:31:51 +0700

    revert code password encrypt

--- addons/auth_crypt/models/res_users.py
+++ addons/auth_crypt/models/res_users.py
@@ -28,7 +28,7 @@ class ResUsers(models.Model):
 
     def init(self):
         # CITIS: tidak perlu hash password tiap kali update, seperlunya aja
-        return  # stop di sini
+        # return  # stop di sini
         _logger.info("Hashing passwords, may be slow for databases with many users...")
         self.env.cr.execute("SELECT id, password FROM res_users"
                    " WHERE password IS NOT NULL"
