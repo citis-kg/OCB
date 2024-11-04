868e9532d15ce562fc0a168f7a78eaa1428c0004
Anwari Rahman <anwari96@gmail.com>
2024-11-04 10:10:16 +0700

    changed odoo error title to server message

--- addons/web/controllers/main.py
+++ addons/web/controllers/main.py
@@ -82,7 +82,7 @@ def serialize_exception(f):
             se = _serialize_exception(e)
             error = {
                 'code': 200,
-                'message': "Odoo Server Error",
+                'message': "Server Message.",  # "Odoo Server Error",
                 'data': se
             }
             return werkzeug.exceptions.InternalServerError(json.dumps(error))
